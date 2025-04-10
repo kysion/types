5315b8734c2811027d76e0e744323fd09dc62097
SupenBysz <supen.huang@qq.com>
2025-04-10 21:37:09 +0800

    refactor(user): 重构用户组件属性接口
    - 移除了未使用的 UserInfoType 导入
    - 重命名 UserOptionType 为 SelectOptionType，并添加泛型参数 T
    - 将 userInfo 属性替换为 data 属性，以适应更广泛的数据类型
    - 更新了 children 属性的类型定义，以避免循环引用问题

--- src/user/props.ts
+++ src/user/props.ts
@@ -1,14 +1,12 @@
-import { UserInfoType } from ".";
-
 export interface BaseOptionType {
     disabled?: boolean;
     className?: string;
     title?: string;
     [name: string]: any;
 }
-export interface UserOptionType extends BaseOptionType {
+export interface SelectOptionType<T> extends BaseOptionType {
     label?: React.ReactNode;
     value?: string | number | null;
-    children?: Omit<UserOptionType, 'children'>[];
-    userInfo: UserInfoType;
+    children?: Omit<SelectOptionType<T>, 'children'>[];
+    data: T;
 }
