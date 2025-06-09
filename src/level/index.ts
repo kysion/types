353f85c02eda890db25df0c19f32d0b0e1f349e2
SupenBysz <supen.huang@qq.com>
2025-06-10 05:23:05 +0800

    feat(会员等级): 添加会员等级相关字段
    添加会员等级模型的新字段，包括等级数值、充值金额限制、下单次数限制、最大金额限制、佣金率以及各种时限设置，用于支持更细粒度的会员权益控制

--- src/level/index.ts
+++ src/level/index.ts
@@ -18,4 +18,22 @@ export class MemberLevelType extends BaseModel<MemberLevelType> {
   createdBy: number = 0;
   // 关联主体 ID，可选
   unionMainId: number = 0;
+  // 权益级别，要遵循数值越大，权益越多，可选
+  level: number = 0;
+  // 充值金额,开通条件，可选
+  rechargeAmount: number = 0;
+  // 日可下单次数，可选
+  canOrderCount: number = 0;
+  // 单笔可做单最大金额，可选
+  canOrderMaxPrice: number = 0;
+  // 佣金率，可选
+  commissionRate: number = 0;
+  // 订单回款最小时限，单位/秒，可选
+  orderDeliveryMinLimitAt: number = 0;
+  // 订单回款最大时限，单位/秒，可选
+  orderDeliveryMaxLimitAt: number = 0;
+  // 接单等待最小时限，单位/秒，可选
+  orderWaitingMinLimitAt: number = 0;
+  // 接单等待最大时限，单位/秒，可选
+  orderWaitingMaxLimitAt: number = 0;
 }
